a1d9daaf86941f2833413af8470676417f4d0eac
melaniaaguilavernetta-gif <melania.aguila.vernetta@gmail.com>
2026-07-31 13:29:51 +0200

    Update ArticleAnsiedad.tsx

--- src/ArticleAnsiedad.tsx
+++ src/ArticleAnsiedad.tsx
@@ -1,5 +1,5 @@
-import { Header } from './Header.tsx';
-import { Footer } from './Footer.tsx';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
 
 export function ArticleAnsiedad() {
   return (
